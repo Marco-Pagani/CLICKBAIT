c669f231d8ba0132a2fbf4fc370d1dc0bf7b25a7
Marco Pagani <marco.pagani@ufl.edu>
2020-01-31 23:38:43 -0500

    Interaction with characters

---
+++ Assets/encounter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class encounter : MonoBehaviour {
+
+    bool is_near = false;
+    private bool action_pressed = false;
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetAxis ("Jump") != 0) {
+            if (!action_pressed) {
+                action_pressed = true;
+                Interact ();
+            }
+        } else {
+            action_pressed = false;
+        }
+    }
+
+    private void OnTriggerEnter (Collider other) {
+        //Debug.Log ("Enter");
+        is_near = true;
+    }
+
+    private void OnTriggerExit (Collider other) {
+        is_near = false;
+    }
+
+    private void Interact () {
+        if (is_near)
+            Debug.Log ("Interact");
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public float speed = 6.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update () {
+        CharacterController controller = GetComponent<CharacterController> ();
+        moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+        moveDirection = transform.TransformDirection (moveDirection);
+        moveDirection *= speed;
+        controller.Move (moveDirection * Time.deltaTime);
+
+        
+    }
+
+}
