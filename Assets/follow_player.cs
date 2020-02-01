7e3ea40673d4ad6c4a7d2440c6065758efe01796
Marco Pagani <marco.pagani@ufl.edu>
2020-02-01 17:06:15 -0500

    Rearranged scene and added camera movement

--- Assets/follow_player.cs
+++ Assets/follow_player.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class follow_player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class follow_player : MonoBehaviour {
+    public Transform target;
+    public int minX = -3, maxX = 3, minY = -5, maxY = 5;
+    void Update () {
+        float x = -target.position.z.Remap (-13, 10, minX, maxX);
+        float y = target.position.x.Remap (-22, 22, minY, maxY);
+        transform.rotation = Quaternion.Euler (66 + x, y, 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+}
+
+public static class ExtensionMethods {
+    //src: https://forum.unity.com/threads/re-map-a-number-from-one-range-to-another.119437/
+    public static float Remap (this float value, float from1, float to1, float from2, float to2) {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
+
 }
