baa0fdbbd841c628d62c7a18b8a61b0acbb8df55
Marco Pagani <marco.pagani@ufl.edu>
2020-02-01 00:08:33 -0500

    enounter code started

---
+++ Assets/encounter_generator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class encounter_generator : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
+
+class EncounterTable {
+    var table;
+    EncounterTable () {
+        table = new Dictionary<string, List<Encounter>> ();
+    }
+
+    void add (string s, Encounter e) {
+        List<Encounter> list;
+
+        if (!dictionary.TryGetValue (s, out list)) {
+            list = new List<Encounter> ();
+            dictionary.Add (s, list);
+        }
+
+        list.Add (e);
+    }
+
+}
+
+class Encounter {
+    string prompt;
+    string[] actions;
+    int[][] effects;
+
+    Encounter (string prompt, string[] actions, int[][] effects) {
+        this.prompt = prompt;
+        this.actions = actions;
+        this.effects = effects;
+    }
+
+}
